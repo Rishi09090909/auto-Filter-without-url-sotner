a3b559bc7d18df6fec005ae4dc47efdf6061254c
Rk_botz <118276768+Rishi09090909@users.noreply.github.com>
2023-01-04 12:18:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Cyniteofficial/Auto-Filter-V5-Without-UrlShortner.git /Auto-Filter-V5-Without-UrlShortner
+  git clone https://github.com/Rishi09090909/auto-Filter-without-url-sotner.git /auto-Filter-without-url-sotner
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-Filter-V5-Without-UrlShortner
+  git clone $UPSTREAM_REPO /auto-Filter-without-url-sotner
 fi
-cd /Auto-Filter-V5
+cd /auto-Filter-without-url-sotner
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
